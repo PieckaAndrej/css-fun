7fe8098caab395e66840c27e026bc49486d286e3
PieckaAndrej <10407548@ucn.dk>
2022-07-26 21:37:16 +0200

    Button 4

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import "./App.css";
 import "./style1.css";
 import "./style2.css";
 import "./style3.css";
+import "./style4.css";
 
 function App() {
-  return (
-    <div className="App">
-		<button className="style1">Button1</button>
-		<button className="style2">Button2</button>
-		<button className="style3">Button3</button>
-    </div>
-  );
+	return (
+		<div className="App">
+			<button className="style1">Button1</button>
+			<button className="style2">Button2</button>
+			<button className="style3">Button3</button>
+			<button className="style4">Button4</button>
+		</div>
+	);
 }
 
 export default App;
